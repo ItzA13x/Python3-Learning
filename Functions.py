0f8c47a2af26df716f8ad317349e2927a6f336e7
Alex Vladiuc <alex.vladiuc@gmail.com>
2023-01-13 17:37:06 +0000

    Functions

--- Functions.py
+++ Functions.py
@@ -1,1 +1,28 @@
 
+# Here’s a simple function named greet_user() that prints a greeting:
+
+def greet_user():
+    """Display a simple greeting."""
+    print("Hello!")
+
+greet_user()
+
+# This example shows the simplest structure of a function. 
+# Line 4 uses the keyword def to inform Python that you’re defining a function. 
+# This is the function definition, which tells Python the name of the function and, if applicable, what kind of information the function needs to do its job. 
+# The parentheses hold that information. 
+# In this case, the name of the function is greet_user(), and it needs no information to do its job, so its parentheses are empty. 
+# (Even so, the parentheses are required.) 
+# Finally, the definition ends in a colon. 
+# Any indented lines that follow def greet_user(): make up the body of the function. 
+# The text at line 5 is a comment called a docstring, which describes what the function does. 
+# Docstrings are enclosed in triple quotes, which Python looks for when it generates documentation for the functions in your programs. 
+# The line print("Hello!") is the only line of actual code in the body of this function, so greet_user() has just one job: print("Hello!"). 
+# When you want to use this function, you call it. 
+# A function call tells Python to execute the code in the function. 
+# To call a function, you write the name of the function, followed by any necessary information in parentheses, as shown at line 8.
+# Because no information is needed here, calling our function is as simple as entering greet_user(). 
+# As expected, it prints Hello!   
+
+####################################################################################################################
+
